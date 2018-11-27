6e8ddd2db842b4e0fa06cd964a00928cd7f79593
Franco Arza <arzafran@gmail.com>
2018-11-27 17:52:42 -0300

    fix apple touch icons

--- pages/_document.js
+++ pages/_document.js
@@ -31,8 +31,38 @@ export default class MyDocument extends Document {
           />
           <link
             rel="apple-touch-icon"
+            sizes="57x57"
+            href="/static/favicon/apple-icon-57x57.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="60x60"
+            href="/static/favicon/apple-icon-60x60.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="72x72"
+            href="/static/favicon/apple-icon-72x72.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="76x76"
+            href="/static/favicon/apple-icon-76x76.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="114x114"
+            href="/static/favicon/apple-icon-114x114.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="152x152"
+            href="/static/favicon/apple-icon-152x152.png"
+          />
+          <link
+            rel="apple-touch-icon"
             sizes="180x180"
-            href="/static/favicon/apple-touch-icon.png"
+            href="/static/favicon/apple-icon-180x180.png"
           />
           <link
             rel="icon"
