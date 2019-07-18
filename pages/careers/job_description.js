2847a1b33bdc1a8f3ad15ca3ad5feff7878789e2
Ginger Collison <ginger@synadia.com>
2019-07-18 16:45:02 -0500

    Update job_description.js

--- pages/careers/job_description.js
+++ pages/careers/job_description.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
 
-export default class Careers extends React.Component {
+export default class Pricing extends React.Component {
   constructor(props) {
     super(props);
   }
