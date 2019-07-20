1fb8e39a28b1bc959d23d405173a8df945bccc3b
Franco Arza <franco@basement.studio>
2019-07-20 10:10:30 -0300

    update builders and serverless compatibility

--- pages/_app.js
+++ pages/_app.js
@@ -6,16 +6,6 @@ import withGA from 'next-ga';
 import settings from '../settings.json';
 
 class MyApp extends App {
-  static async getInitialProps({ Component, router, ctx }) {
-    let pageProps = {};
-
-    if (Component.getInitialProps) {
-      pageProps = await Component.getInitialProps(ctx);
-    }
-
-    return { pageProps };
-  }
-
   render() {
     const { Component, pageProps } = this.props;
     return (
